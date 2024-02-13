e2a92aa2332302944bcc4cbc39d7de84c5fea471
Senu-sesi2 <senu.sesi2@gmail.com>
2024-02-13 21:50:30 +0000

    The model files

---
+++ models/state.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This defines  stateModel class
+"""
+
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    # state model
+
+    # Atrributes
+    name: str = ''
